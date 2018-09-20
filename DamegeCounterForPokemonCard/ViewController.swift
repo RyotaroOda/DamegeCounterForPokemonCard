476bb3b9463853ae1ea1a61c3b3487ba5fd05f32
小田　竜太郎 <odaryotaro@odaryuutarou-no-macbook-air.local>
2018-09-20 10:32:55 +0900

    Initial Commit

---
+++ DamegeCounterForPokemonCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DamegeCounterForPokemonCard
+//
+//  Created by 小田　竜太郎 on 2018/09/20.
+//  Copyright © 2018年 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
